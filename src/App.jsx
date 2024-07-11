23a3b802689371bab5be659c6bea9e55fed44716
Alexey Vodoley <Alex-vodolei@rambler.ru>
2024-07-11 11:25:16 +0400

    feat: refactor App

--- src/App.jsx
+++ src/App.jsx
@@ -33,22 +33,22 @@ function App() {
               <h3>Menu:</h3>
               <ul>
                   <li>
-                      {restaurants[0].menu[0].name}
+                      {restaurants[1].menu[0].name}
                   </li>
                   <li>
-                      {restaurants[0].menu[1].name}
-                  </li>
-                  <li>
-                      {restaurants[0].menu[2].name}
+                      {restaurants[1].menu[1].name}
                   </li>
               </ul>
               <h3>Reviews:</h3>
               <ul>
                   <li>
-                      {restaurants[0].reviews[0].text}
+                      {restaurants[1].reviews[0].text}
                   </li>
                   <li>
-                      {restaurants[0].reviews[1].text}
+                      {restaurants[1].reviews[1].text}
+                  </li>
+                  <li>
+                      {restaurants[1].reviews[2].text}
                   </li>
               </ul>
           </div>
@@ -57,22 +57,19 @@ function App() {
               <h3>Menu:</h3>
               <ul>
                   <li>
-                      {restaurants[0].menu[0].name}
+                      {restaurants[2].menu[0].name}
                   </li>
                   <li>
-                      {restaurants[0].menu[1].name}
+                      {restaurants[2].menu[1].name}
                   </li>
                   <li>
-                      {restaurants[0].menu[2].name}
+                      {restaurants[2].menu[2].name}
                   </li>
               </ul>
               <h3>Reviews:</h3>
               <ul>
                   <li>
-                      {restaurants[0].reviews[0].text}
-                  </li>
-                  <li>
-                      {restaurants[0].reviews[1].text}
+                      {restaurants[2].reviews[0].text}
                   </li>
               </ul>
           </div>
@@ -81,22 +78,19 @@ function App() {
               <h3>Menu:</h3>
               <ul>
                   <li>
-                      {restaurants[0].menu[0].name}
+                      {restaurants[3].menu[0].name}
                   </li>
                   <li>
-                      {restaurants[0].menu[1].name}
-                  </li>
-                  <li>
-                      {restaurants[0].menu[2].name}
+                      {restaurants[3].menu[1].name}
                   </li>
               </ul>
               <h3>Reviews:</h3>
               <ul>
                   <li>
-                      {restaurants[0].reviews[0].text}
+                      {restaurants[3].reviews[0].text}
                   </li>
                   <li>
-                      {restaurants[0].reviews[1].text}
+                      {restaurants[3].reviews[1].text}
                   </li>
               </ul>
           </div>
